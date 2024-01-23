8f1b0c2e1191874407740901ce20213655757572
MATHEUS CHAVES ARAUJO <002919@cesed.local>
2024-01-23 11:16:09 -0300

    Login feature

--- src/lib/validators/account-credentials.ts
+++ src/lib/validators/account-credentials.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const AuthCredentials = z.object({
     email: z.string().email(),
-    password: z.string().min(8, {message: 'Password must be at least 8 characters long'})
+    password: z.string().min(6, {message: 'Password must be at least 8 characters long'})
 })
 
 export type TAuthCredentials = z.infer<typeof AuthCredentials>
--- src/trcp/index.ts
+++ src/trcp/index.ts
@@ -1,7 +1,8 @@
+import { authrouter } from "./auth-router";
 import { router } from "./trpc";
 
 export const appRouter = router({
-
+    auth: authrouter
 })
 
 export type AppRouter = typeof appRouter;
